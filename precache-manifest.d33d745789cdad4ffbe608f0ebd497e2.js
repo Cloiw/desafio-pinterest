b7b760e64046983cdd4bee814c99c944c94163c7
Cloiw <sircli@hotmail.com>
2019-08-14 17:09:21 -0400

    Updates

---
+++ precache-manifest.d33d745789cdad4ffbe608f0ebd497e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f87602ee2807e6ac3873e034addbbb",
+    "url": "/desafio-pinterest/index.html"
+  },
+  {
+    "revision": "64acb15036784db0a52e",
+    "url": "/desafio-pinterest/static/css/main.43a06807.chunk.css"
+  },
+  {
+    "revision": "6a607da4c88afe156778",
+    "url": "/desafio-pinterest/static/js/2.86302cab.chunk.js"
+  },
+  {
+    "revision": "64acb15036784db0a52e",
+    "url": "/desafio-pinterest/static/js/main.db4cf954.chunk.js"
+  },
+  {
+    "revision": "09f0edb94085fd85ba11",
+    "url": "/desafio-pinterest/static/js/runtime~main.9cc5fae6.js"
+  },
+  {
+    "revision": "84d16ae0e395784e0e4704d9bcbdaa86",
+    "url": "/desafio-pinterest/static/media/logo.84d16ae0.svg"
+  }
+]);
