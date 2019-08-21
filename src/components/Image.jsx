8a9478b97e97eb9e7f3b4d6e4e1073be17f02c4c
Cloiw <sircli@hotmail.com>
2019-08-21 02:58:23 -0400

    agregando boton guardar

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import './styles/Body.css';
+import './styles/Image.css';
 import dotsIcon from '../img/dots_icon.png';
+import saveIcon from '../img/save_icon.jpg';
 
-const Image = (props) => (
-  <div className="image-div">
-    <img className="image-size" src={props.url} alt={props.url} />
-    <div className="flex-dots">
-      <img className="image-dots" src={dotsIcon} alt="More" />
-    </div>
-  </div>
-);
+class Image extends React.Component {
+  constructor() {
+    super();
+    this.state = { isHover: false };
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover = () => {
+    this.setState((prevState) => (
+      { isHover: !prevState.isHover }
+    ));
+  };
+
+  render() {
+    return (
+      <>
+        <div onMouseEnter={() => this.handleHover()} onMouseLeave={() => this.handleHover()} className="image-div">
+          <button type="button" className={this.state.isHover ? 'show-save-button' : 'hide-save-button'}>
+            <img className="save-icon" src={saveIcon} alt="Guardar" />
+          </button>
+          <img className="image-size" src={this.props.url} alt={this.props.url} />
+          <div className="flex-dots">
+            <img className="image-dots" src={dotsIcon} alt="More" />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Image;
